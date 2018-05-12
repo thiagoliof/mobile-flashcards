93515e2188c3b95797604706066833b3650e3fcf
Thiago Abreu <thiago.abreu@gmail.com>
2018-05-12 11:32:56 -0300

    Inicio flip

--- components/Quiz.js
+++ components/Quiz.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux'
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 import { itemDetails } from '../utils/helpers';
+
+import FlipCard from 'react-native-flip-card'
 
 
 class Quiz extends React.Component {
@@ -11,13 +13,58 @@ class Quiz extends React.Component {
 		const { item } = this.props.navigation.state.params
         const details = itemDetails(deck, item.title)
 		return (
-			<View>
+			<View style={styles.container}>
 				<View><Text>1 de {details.questions.length}</Text></View>
-				<View></View>
-            </View>
-		);
-	}
+				<FlipCard 
+					style={styles.flipCard}
+					friction={6}
+					perspective={1000}
+					flipHorizontal={false}
+					flipVertical={true}
+					flip={true}
+					clickable={true}
+					onFlipEnd={(isFlipEnd)=>{console.log('isFlipEnd', isFlipEnd)}}
+					>
+					{/* Face Side */}
+					<View >
+						<Text>The Face</Text>
+					</View>
+					{/* Back Side */}
+					<View>
+						<Text>The Back</Text>
+					</View>
+					</FlipCard>
+			</View>
+							);
+		}
 }
+
+const styles = StyleSheet.create({
+	container: {
+	  flex: 1,
+	  alignItems: "center",
+	  justifyContent: "center",
+	},
+	flipCard: {
+	  width: 500,
+	  height: 500,
+	  alignItems: 'center',
+	  justifyContent: 'center',
+	  backgroundColor: 'blue',
+	  backfaceVisibility: 'hidden',
+	},
+	flipCardBack: {
+	  backgroundColor: "red",
+	  position: "absolute",
+	  top: 0,
+	},
+	flipText: {
+	  width: 90,
+	  fontSize: 20,
+	  color: 'white',
+	  fontWeight: 'bold',
+	}
+  });
 
 function mapStateToProps ({ deck }) {
     return {
