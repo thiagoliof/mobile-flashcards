b05e9d0a8c25620a4bd2a96644679fdbe09d5356
Thiago Abreu <thiago.abreu@gmail.com>
2018-05-14 17:51:15 -0300

    Commit to submit

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,44 +1,57 @@
-export function getDeckMetaInfo (deck) {
-    const info = {
-      React: {
-        title: 'React',
-        questions: [
-          {
-            question: 'What is React?',
-            answer: 'A library for managing user interfaces'
-          },
-          {
-            question: 'Where do you make Ajax requests in React?',
-            answer: 'The componentDidMount lifecycle event'
-          }
-        ]
-      },
-      JavaScript: {
-        title: 'JavaScript',
-        questions: [
-          {
-            question: 'What is a closure?',
-            answer: 'The combination of a function and the lexical environment within which that function was declared.'
-          }
-        ]
-      },
-      JavaScript2: {
-        title: 'JavaScript2',
-        questions: [
-          {
-            question: 'What is a closure?',
-            answer: 'The combination of a function and the lexical environment within which that function was declared.'
-          }
-        ]
-      },
-      
-    }
-  
-    return typeof deck === 'undefined'
-      ? info
-      : info[deck]
-  }
+import React from 'react';
+import { AsyncStorage  } from 'react-native';
+import { Notifications, Permissions } from 'expo'
 
-  export function itemDetails(metaInfo, key){
-    return metaInfo[key]
-  }
+const NOTIFICATION_KEY = 'Flascards:notifications'
+
+export function getDeckMetaInfo (deck) {
+	return typeof deck === 'undefined'
+		? info
+		: info[deck]
+}
+
+export function itemDetails(metaInfo, key){
+	return metaInfo[key]
+}
+
+export function clearLocalNotification(){
+	return AsyncStorage.removeItem(NOTIFICATION_KEY)
+		.then(Notifications.cancelAllScheduledNotificationsAsync())
+} 
+function createNotification(){
+	return {
+		title: 'Lembrete de estudo',
+		body: 'Não esqueça de estudar no dia de hoje',
+		ios: {
+			sound: true,
+		},
+	}
+}
+export function setLocalNotification(){
+	AsyncStorage.getItem(NOTIFICATION_KEY)
+		.then(JSON.parse)
+		.then((data) =>{
+			if(data === null){
+				Permissions.askAsync(Permissions.NOTIFICATIONS)
+					.then(({ status }) =>{
+						if (status === 'granted'){
+							Notifications.cancelAllScheduledNotificationsAsync()
+							
+							let tomorrow = new Date()
+							tomorrow.setDate( tomorrow.getDate() + 1 )
+							tomorrow.setHours(20)
+							tomorrow.setMinutes(0)
+
+							Notifications.scheduleLocalNotificationAsync(
+								createNotification(),
+								{
+									time: tomorrow,
+									repeat: 'day' 
+								}
+							)
+							AsyncStorage.setItem(NOTIFICATION_KEY, JSON.stringify(true))
+						}
+					})
+			}
+		})
+}
